105e0ca805bfc026d569c49cd49ac23cf9578550
Francis Gulotta <wizard@roborooter.com>
2021-12-22 15:35:21 -0500

    day 4 with debug info

--- src/day4.rs
+++ src/day4.rs
@@ -187,9 +187,18 @@ impl Game {
 
     fn play(&mut self) -> Option<(BingoBoard, i32)> {
         for num in self.moves.as_slice() {
-            for board in self.boards.iter_mut() {
+            for (id, board) in self.boards.iter_mut().enumerate() {
+                if board.check_for_win().is_some() {
+                    continue;
+                }
+                println!("playing {} on board {}", num, id);
                 match board.play(*num) {
-                    Some(product) => return Some((board.clone(), product)),
+                    Some(product) => {
+                        let sum = board.check_for_win().unwrap_or(-1);
+                        println!("{:?}", board);
+                        println!("Board {} won with {}, a sum of {} and a product of {}!", id, num, sum, product);
+                        // return Some((board.clone(), product));
+                    },
                     None => {}
                 }
             }
@@ -204,26 +213,7 @@ mod game_tests {
 
   #[test]
   fn sample_game() {
-    let input = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
-
-22 13 17 11  0
-8  2 23  4 24
-21  9 14 16  7
-6 10  3 18  5
-1 12 20 15 19
-
-3 15  0  2 22
-9 18 13 17  5
-19  8  7 25 23
-20 11 10 24  4
-14 21 16 12  6
-
-14 21 17 24  4
-10 16 15  9 19
-18  8 23 26 20
-22 11 13  6  5
-2  0 12  3  7
-".to_string();
+    let input = include_str!("day-04-sample.txt");
 
     let lines = input.lines().map(|s| s.to_string());
     let mut game = Game::new_from_lines(lines);
